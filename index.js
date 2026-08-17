267fb9b6af672674eb47d780f569a4e341d0b448
Novaire-Tester <vjacob1127@gmail.com>
2026-08-17 16:11:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,28 @@ const client = new Client({
     ]
 });
 
+// This object will temporarily hold active boosters while the bot stays online
+const activeBoosters = {};
+
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     client.user.setActivity('!test', { type: ActivityType.Listening });
+
+    // Check every hour (3600000ms) to see if it's time to run the daily command
+    setInterval(() => {
+        const now = Date.now();
+        for (const userId in activeBoosters) {
+            const data = activeBoosters[userId];
+            // If 24 hours (86400000ms) have passed since the last run
+            if (now - data.lastRun >= 86400000) {
+                const channel = client.channels.cache.get(data.channelId);
+                if (channel) {
+                    channel.send(`;add-money <@${userId}> 50,000`);
+                    data.lastRun = now; // Reset the 24-hour timer
+                }
+            }
+        }
+    }, 3600000); 
 });
 
 client.on('messageCreate', (message) => {
@@ -24,6 +43,25 @@ client.on('messageCreate', (message) => {
     // Command 2: !help
     else if (message.content === '!help') {
         message.reply('Hey! Im MTH Bot, I usually take care of Moderation, Rewards, and More!');
+    }
+
+    // Command 3: !Booster-1 @person
+    else if (message.content.startsWith('!Booster-1')) {
+        const targetUser = message.mentions.users.first();
+        
+        if (!targetUser) {
+            return message.reply('Uh oh! Please ping a person! Example: `!Booster-1 @person`');
+        }
+
+        // Add them to the daily cycle
+        activeBoosters[targetUser.id] = {
+            channelId: message.channel.id,
+            lastRun: Date.now() // Sets the starting point to right now
+        };
+
+        // Send the very first immediate message right now
+        message.channel.send(`;add-money ${targetUser} 50,000`);
+        message.reply(`✓ Added ${targetUser} to the Booster 1 Perks list. I will repeat this command every 24 hours!`);
     }
 });
 
